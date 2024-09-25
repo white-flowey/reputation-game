Config change. Pass initial values to agents via dict, not list. Added characters, fixed weight calculation for topic and listeners.

simulate/agent/agent.py
@@ -44,7 +44,7 @@ class Agent:
         """Sets up the agent's character attributes based on the provided character mapping e.g., self.deceptive."
 
         Args:
-            character (str): Character traits of the agent.
+            character (str): Character string of the agent.
         """
         for key, value in character_mapping(character).items():
             setattr(self, key, value)
@@ -54,16 +54,18 @@ class Agent:
         n_agents = self.conf("n_agents")
         self.friendships = [Info(1, 0) if i == self.id else Info(0, 0) for i in agents]
         self.I = [Info(0, 0)] * n_agents
-        self.Iothers = [[Info(0, 0)] * n_agents for _ in range(n_agents)]  # Changed to avoid reference issue
-        self.J = [[Info(0, 0)] * n_agents for _ in range(n_agents)]  # Changed to avoid reference issue
-        self.C = [[Info(0, 0)] * n_agents for _ in range(n_agents)]  # Changed to avoid reference issue
+        self.Iothers = [[Info(0, 0)] * n_agents for _ in range(n_agents)]
+        self.J = [[Info(0, 0)] * n_agents for _ in range(n_agents)]
+        self.C = [[Info(0, 0)] * n_agents for _ in range(n_agents)]
         self.K = [np.sqrt(np.pi)] * self.conf("KLENGTH")
         self.kappa = 1
-        self.n_conversations = [{"partner": 0, "topic": 0} for _ in range(n_agents)]  # Changed to avoid reference issue
+        self.n_conversations = [{"partner": 0, "topic": 0} for _ in range(n_agents)]
 
     def initialise_memory(self) -> None:
         """Initializes the agent's memory with predefined values from the configuration."""
-        if self.conf("mindI"):
-            self.I = [Info(self.conf("mindI")[b][0], self.conf("mindI")[b][1]) for b in agents]
-        self.K = self.conf("Ks")[self.id] if self.conf("Ks") else [np.sqrt(np.pi) for _ in range(self.conf("KLENGTH"))]
-        self.kappa = median(self.K) / np.sqrt(np.pi) if self.conf("Ks") else 1
+        if self.id in self.conf("mindI_dict"):
+            for b in self.conf("mindI_dict")[self.id].keys():
+                self.I[b] = Info(self.conf("mindI_dict")[self.id][b][0], self.conf("mindI_dict")[self.id][b][1])
+        if self.id in self.conf("Ks_dict"):
+            self.K = self.conf("Ks_dixt")[self.id] if self.conf("Ks_dict") else [np.sqrt(np.pi) for _ in range(self.conf("KLENGTH"))]
+        self.kappa = median(self.K) / np.sqrt(np.pi)

simulate/agent/initiator.py
@@ -55,10 +55,13 @@ class Initiator:
             return self.a.id
 
         agents = self.a.conf("agents")
-        rel_weights = (np.array([self.a.n_conversations[i]["partner"] for i in agents]) ** 
-                       self.a.conf("RELATION_AFFECTS_C") ** self.a.shyness)
+
         friend_weights = np.array([self.a.friendships[i].mean for i in agents]) ** self.a.conf("FRIENDSHIP_AFFECTS_C")
-        agg_weights = (1 - friend_weights) ** self.a.aggressive > self.a.random["aggressive"].uniform()
+        rel_weights = (np.array([self.a.n_conversations[i]["topic"] for i in agents]) +
+                        np.array([self.a.n_conversations[i]["partner"] for i in agents]) * self.a.conf("Q")) ** self.a.conf("RELATION_AFFECTS_C")
+        
+        rel_weights = rel_weights * friend_weights * self.a.shyness
+        agg_weights = (1 - friend_weights) ** (self.a.aggressive > self.a.random["aggressive"].uniform())
         weights = rel_weights * friend_weights * agg_weights  
         return draw_max_from_list(self.a.random, weights, "topic")
 
@@ -72,13 +75,15 @@ class Initiator:
             np.ndarray: An array of weights representing the ranking of the listeners.
         """
         friend_weights = np.array([self.a.friendships[i].mean for i in others]) ** self.a.conf("FRIENDSHIP_AFFECTS_B")
-        rel_weights = (friend_weights * np.array([self.a.n_conversations[i]["partner"] for i in others]) ** 
-                       self.a.conf("RELATION_AFFECTS_B") ** self.a.shyness)
+        rel_weights = (np.array([self.a.n_conversations[i]["topic"] for i in others]) +
+                        np.array([self.a.n_conversations[i]["partner"] for i in others]) * self.a.conf("Q")) ** self.a.conf("RELATION_AFFECTS_B")
+        rel_weights = rel_weights * friend_weights * self.a.shyness
 
         strat_weights = np.array([(self.a.I[b].mean if self.a.strategic > 0 else 1 - self.a.I[b].mean) 
                                    ** abs(self.a.strategic) for b in others])
         weights = rel_weights * strat_weights
-        return weights / weights.sum()  # Normalize the weights
+        weights_sum = weights.sum()
+        return weights / weights_sum if weights_sum > 0 else weights  # Normalize the weights
 
     def draw_topic(self, agents: list[int]) -> int:
         """Draws a topic based on the weights derived from relationships and friendships.

simulate/simulation.py
@@ -46,14 +46,14 @@ class Simulation:
         The random dict is simulation-specific, thus passed onto the simulation's agents.
         """
         agents = self.conf("agents")
-        honesties = (self.conf("honesties") or 
+        honesties = (self.conf("honesties_dict") or 
                     sorted([Info(0, 0).draw() for _ in agents]) if self.conf("RANDOM_HONESTIES") 
                     else np.linspace(0, 1, self.conf("n_agents")))
 
         characters = [self.characters_setup.get(agent, self.characters_setup["all"]) for agent in agents]
         
-        self.agents = [Agent(id, honesty, character, self.random, self.log, self.conf) 
-                        for id, honesty, character in zip(agents, honesties, characters)]
+        self.agents = [Agent(id, honesties[id], character, self.random, self.log, self.conf) 
+                        for id, character in zip(agents, characters)]
     
     def schedule_conversations(self) -> list[Conversation]:
         """