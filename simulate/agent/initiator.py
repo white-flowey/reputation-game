6b9d9561124a658be2910d1ee7ebe25a6364320a
MaiBe-ctrl <maiisabensalah@gmail.com>
2024-12-01 14:52:32 +0100

    added villager_1

--- simulate/agent/initiator.py
+++ simulate/agent/initiator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from helper.help_conversation import draw_max_from_list
 
+
 class Initiator:
     """
     Responsible for initiating conversations by selecting settings, listeners, and topics based on agent characteristics.
@@ -20,7 +21,11 @@ class Initiator:
         Returns:
             str: The chosen setting, either "one_to_one" or "one_to_all".
         """
-        return "one_to_one" if self.a.random["n_recipients"].uniform() <= self.a.conf("p_one_to_one") else "one_to_all"
+        return (
+            "one_to_one"
+            if self.a.random["n_recipients"].uniform() <= self.a.conf("p_one_to_one")
+            else "one_to_all"
+        )
 
     def pick_listeners(self, setting: str) -> dict:
         """Selects listeners based on the conversation setting and the agent's character.
@@ -34,9 +39,23 @@ class Initiator:
         others = [id for id in self.a.conf("agents") if id != self.a.id]
         if setting == "one_to_one":
             if self.a.character in ["ordinary", "werewolf", "villager"]:
-                return {"ids": [self.a.random["recipients"].choice(others)], "weights": [1]}
+                return {
+                    "ids": [self.a.random["recipients"].choice(others)],
+                    "weights": [1],
+                }
+            if self.a.character == "villager_1":
+                listeners = {
+                    "ids": others,
+                    "weights": self.rank_listeners_villagers(others),
+                }
+                max_weight_id = draw_max_from_list(
+                    self.a.random, listeners["weights"], "recipients"
+                )
+                return {k: [v[max_weight_id]] for k, v in listeners.items()}
             listeners = {"ids": others, "weights": self.rank_listeners(others)}
-            max_weight_id = draw_max_from_list(self.a.random, listeners["weights"], "recipients")
+            max_weight_id = draw_max_from_list(
+                self.a.random, listeners["weights"], "recipients"
+            )
             return {k: [v[max_weight_id]] for k, v in listeners.items()}
 
         weights = self.rank_listeners(others)
@@ -53,16 +72,26 @@ class Initiator:
 
         if self.a.egocentric > self.a.random["egocentric"].uniform():
             return self.a.id
+        others = [id for id in self.a.conf("agents") if id != self.a.id]
+        if self.a.character == "villager_1":
+            return self.pick_topic_villagers(others)
 
         agents = self.a.conf("agents")
 
-        friend_weights = np.array([self.a.friendships[i].mean for i in agents]) ** self.a.conf("FRIENDSHIP_AFFECTS_C")
-        rel_weights = (np.array([self.a.n_conversations[i]["topic"] for i in agents]) +
-                        np.array([self.a.n_conversations[i]["partner"] for i in agents]) * self.a.conf("Q")) ** self.a.conf("RELATION_AFFECTS_C")
-        
+        friend_weights = np.array(
+            [self.a.friendships[i].mean for i in agents]
+        ) ** self.a.conf("FRIENDSHIP_AFFECTS_C")
+        rel_weights = (
+            np.array([self.a.n_conversations[i]["topic"] for i in agents])
+            + np.array([self.a.n_conversations[i]["partner"] for i in agents])
+            * self.a.conf("Q")
+        ) ** self.a.conf("RELATION_AFFECTS_C")
+
         rel_weights = rel_weights * friend_weights * self.a.shyness
-        agg_weights = (1 - friend_weights) ** (self.a.aggressive > self.a.random["aggressive"].uniform())
-        weights = rel_weights * friend_weights * agg_weights  
+        agg_weights = (1 - friend_weights) ** (
+            self.a.aggressive > self.a.random["aggressive"].uniform()
+        )
+        weights = rel_weights * friend_weights * agg_weights
         return draw_max_from_list(self.a.random, weights, "topic")
 
     def rank_listeners(self, others: list[int]) -> np.ndarray:
@@ -74,16 +103,51 @@ class Initiator:
         Returns:
             np.ndarray: An array of weights representing the ranking of the listeners.
         """
-        friend_weights = np.array([self.a.friendships[i].mean for i in others]) ** self.a.conf("FRIENDSHIP_AFFECTS_B")
-        rel_weights = (np.array([self.a.n_conversations[i]["topic"] for i in others]) +
-                        np.array([self.a.n_conversations[i]["partner"] for i in others]) * self.a.conf("Q")) ** self.a.conf("RELATION_AFFECTS_B")
+        friend_weights = np.array(
+            [self.a.friendships[i].mean for i in others]
+        ) ** self.a.conf("FRIENDSHIP_AFFECTS_B")
+        rel_weights = (
+            np.array([self.a.n_conversations[i]["topic"] for i in others])
+            + np.array([self.a.n_conversations[i]["partner"] for i in others])
+            * self.a.conf("Q")
+        ) ** self.a.conf("RELATION_AFFECTS_B")
         rel_weights = rel_weights * friend_weights * self.a.shyness
 
-        strat_weights = np.array([(self.a.I[b].mean if self.a.strategic > 0 else 1 - self.a.I[b].mean) 
-                                   ** abs(self.a.strategic) for b in others])
+        strat_weights = np.array(
+            [
+                (self.a.I[b].mean if self.a.strategic > 0 else 1 - self.a.I[b].mean)
+                ** abs(self.a.strategic)
+                for b in others
+            ]
+        )
         weights = rel_weights * strat_weights
         weights_sum = weights.sum()
-        return weights / weights_sum if weights_sum > 0 else weights  # Normalize the weights
+        return (
+            weights / weights_sum if weights_sum > 0 else weights
+        )  # Normalize the weights
+
+    def rank_listeners_villagers(self, others: list[int]) -> np.ndarray:
+        partners = np.array([self.a.n_conversations[i]["partner"] for i in others])
+        if np.sum(partners) <= (2 * self.a.conf("n_rounds") / 3):
+            min_index = np.argmin(partners)
+            partners = np.zeros_like(partners, dtype=int)
+            partners[min_index] = 1
+            return partners
+        else:
+            max_truthfulness = -1
+            id_truthfulness = self.a.id
+            for id, info in zip(self.a.conf("agents"), self.a.I):
+                curr_truthfulness = info.mu / (info.mu + info.la)
+                if curr_truthfulness > max_truthfulness and id != self.a.id:
+                    max_truthfulness = curr_truthfulness
+                    id_truthfulness = id
+            return np.array([1 if o == id_truthfulness else 0 for o in others])
+
+    def pick_topic_villagers(self, others: list[int]) -> int:
+        topics = np.array([self.a.n_conversations[i]["topic"] for i in others])
+        min_index = np.argmin(topics)
+
+        return others[min_index]
 
     def draw_topic(self, agents: list[int]) -> int:
         """Draws a topic based on the weights derived from relationships and friendships.
@@ -94,9 +158,15 @@ class Initiator:
         Returns:
             int: The ID of the drawn topic.
         """
-        rel_weights = np.array([self.a.n_conversations[i]["partner"] for i in agents]) ** self.a.conf("RELATION_AFFECTS_C")
-        friend_weights = np.array([self.a.friendships[i].mean for i in agents]) ** self.a.conf("FRIENDSHIP_AFFECTS_C")
-        agg_weights = (1 - friend_weights) ** (self.a.aggressive > self.a.random["aggressive"].uniform())
+        rel_weights = np.array(
+            [self.a.n_conversations[i]["partner"] for i in agents]
+        ) ** self.a.conf("RELATION_AFFECTS_C")
+        friend_weights = np.array(
+            [self.a.friendships[i].mean for i in agents]
+        ) ** self.a.conf("FRIENDSHIP_AFFECTS_C")
+        agg_weights = (1 - friend_weights) ** (
+            self.a.aggressive > self.a.random["aggressive"].uniform()
+        )
 
         weights = rel_weights * friend_weights * agg_weights
         return agents[draw_max_from_list(self.a.random, weights, "topic")]
